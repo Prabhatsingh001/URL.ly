2e2d97bfb9e23ba47528595f241f27ad7a523e28
Prabhat <87269080+Prabhatsingh001@users.noreply.github.com>
2025-07-13 23:22:56 +0530

    changed settings

--- UrlShortner/UrlShortner/settings.py
+++ UrlShortner/UrlShortner/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1", "url-shortner-if03.onrender.com"]
+ALLOWED_HOSTS = ["127.0.0.1", ".onrender.com"]
 
 
 # Application definition
