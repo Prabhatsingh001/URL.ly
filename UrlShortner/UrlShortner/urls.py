118c59d1bf40d7c251dbfb71cf22f5b723dbbdf8
Prabhat <87269080+Prabhatsingh001@users.noreply.github.com>
2025-07-13 23:43:57 +0530

    done

--- UrlShortner/UrlShortner/urls.py
+++ UrlShortner/UrlShortner/urls.py
@@ -29,7 +29,7 @@ urlpatterns = [
     path("accounts/", include(("Auth.urls", "Auth"), namespace="accounts")),
     path("url/", include(("urlLogic.urls", "urlLogic"), namespace="url")),
     # Tailwind CSS
-    path("__reload__/", include("django_browser_reload.urls")),
+    # path("__reload__/", include("django_browser_reload.urls")),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
