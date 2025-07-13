b3f6e15975af2247c0865faec27d4b38c2a35cd4
Prabhat <87269080+Prabhatsingh001@users.noreply.github.com>
2025-07-13 15:53:56 +0530

    new file

--- build_files.sh
+++ build_files.sh
@@ -1,1 +1,17 @@
+#!/bin/bash
+
+# Install Python dependencies
 pip install -r requirements.txt
+
+# Install Tailwind CSS (uses Node + Tailwind CLI)
+python manage.py tailwind install
+
+# Build Tailwind CSS
+python manage.py tailwind build
+
+# Collect static files for deployment
+python manage.py collectstatic --noinput
+
+# Move static files to Vercel's expected output folder
+mkdir -p staticfiles_build
+cp -r staticfiles/* staticfiles_build/
